da9f868d42070a9d75753f1f5f1f83b766187155
LaloLopez <alexlopez444@gmail.com>
2013-12-28 14:24:31 -0430

    Mi primer commit IUTY 2013

---
+++ hola_mundo.c
@@ -0,0 +1,14 @@
+// Programa: hola_mundo.c
+// Autor: Alexander Lopez
+// Version: 1.0
+// Fecha: 28/12/2013
+// Descripcion: Programa que muestra una cadena de texto por la salida estandar
+
+
+#include <stdio.h>
+
+int main(void)
+{
+    printf("Hola Mundo remoto desde GitHub.com\n");
+    return 0;
+}
